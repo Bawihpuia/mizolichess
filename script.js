fe132fe66996924aac40e9cad7919a2eb026c3d8
Lalremruatfela Chhakchhuak <31438072+Bawihpuia@users.noreply.github.com>
2022-08-03 21:22:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const tokenEndpoint = "https://lichess.org/api/token";
                 response_type: "code",
                 client_id: clientId,
                 code_challenge_method: "S256",
-                scope: "challenge:write",
+                scope: "challenge:write team:write",
                 code_challenge: codeChallenge,
                 redirect_uri: redirectUri
             });
